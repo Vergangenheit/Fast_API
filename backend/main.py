5592e015b1583b7419de98987f8844cffc7f93d3
Vergangenheit <lorenzo.ostano@gmail.com>
2020-10-06 20:35:07 +0200

    added api routes

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,9 @@ from fastapi import FastAPI
 from fastapi import UploadFile
 import numpy as np
 import cv2
+import config
 from PIL import Image
+import inference
 
 app = FastAPI()
 
@@ -15,12 +17,15 @@ app = FastAPI()
 def read_root():
     return {"message": "welcome to the api"}
 
+
 @app.post("/{style}")
 def get_image(style: str, file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
-    model = None
-    output, resized = None, None
+    model = config.STYLES[style]
+    output, resized = inference.inference(model, image)
+    name = f"/storage/{str(uuid.uuid4())}.jpg"
+    cv2.imwrite(name, output)
+    return {"name": name}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080)
-
